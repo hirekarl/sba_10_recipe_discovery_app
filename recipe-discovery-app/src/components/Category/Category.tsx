16b51bbe082f4e165ce0a9b7c26059f03743148b
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-09 18:19:51 -0400

    Added keys to list items.

--- recipe-discovery-app/src/components/Category/Category.tsx
+++ recipe-discovery-app/src/components/Category/Category.tsx
@@ -1,5 +1,37 @@
+import { Link, Navigate, useParams } from "react-router-dom"
+import { useFetch } from "../../hooks/useFetch"
+import type { APIRecipesType } from "../../types"
+import type { ReactNode } from "react"
+
 const Category = () => {
-  return <div>Category</div>
+  const { categoryName } = useParams()
+  const { data, loading, error } = useFetch({
+    type: "filter",
+    term: categoryName as string,
+  })
+
+  if (!categoryName) return <Navigate to="/not-found" />
+
+  let recipes: ReactNode = null
+
+  if (data) {
+    recipes = (data as APIRecipesType).meals.map((meal) => (
+      <li key={meal.idMeal}>
+        <Link to={`/recipe/${meal.idMeal}`}>{meal.strMeal}</Link>
+      </li>
+    ))
+  }
+
+  if (!data && !loading) return <Navigate to="/not-found" />
+
+  return (
+    <>
+      <h1>{categoryName}</h1>
+      {loading && <div>Loading&hellip;</div>}
+      {error && <div className="text-danger">{error}</div>}
+      <ul>{recipes}</ul>
+    </>
+  )
 }
 
 export default Category
