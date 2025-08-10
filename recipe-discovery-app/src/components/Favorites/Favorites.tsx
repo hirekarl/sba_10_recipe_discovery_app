b8e611571dab92b8345adfd7ccc667720d40f06c
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-10 18:58:56 -0400

    Implemented Favorites page.

--- recipe-discovery-app/src/components/Favorites/Favorites.tsx
+++ recipe-discovery-app/src/components/Favorites/Favorites.tsx
@@ -1,5 +1,35 @@
+import { useContext, type ReactNode } from "react"
+import FavoritesContext, {
+  type FavoritesContextType,
+} from "../../contexts/FavoritesContext"
+import RecipeCard from "../Recipe/RecipeCard"
+
 const Favorites = () => {
-  return <div>Favorites</div>
+  const { favorites } = useContext<FavoritesContextType>(FavoritesContext)
+
+  let recipeCards: ReactNode
+  if (favorites) {
+    recipeCards = favorites.map((recipe) => (
+      <div key={recipe.idMeal} className="col-lg-3 col-md-4 col-sm-6 mb-3">
+        <RecipeCard recipe={recipe} />
+      </div>
+    ))
+  }
+  return (
+    <>
+      {favorites && (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+              <div className="my-4"></div>
+              <h1 className="text-center my-4">Favorite Recipes</h1>
+              <div className="row">{recipeCards}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Favorites
